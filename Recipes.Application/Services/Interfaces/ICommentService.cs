2a4cc12610b0499ab787df083e79def652be03ee
antspring <antspring@users.noreply.github.com>
2026-04-24 01:15:10 +0500

    feat: update service for crud with new images field

--- Recipes.Application/Services/Interfaces/ICommentService.cs
+++ Recipes.Application/Services/Interfaces/ICommentService.cs
@@ -4,11 +4,12 @@ namespace Recipes.Application.Services.Interfaces;
 
 public interface ICommentService
 {
-    Task<CommentDto> CreateCommentAsync(Guid recipeId, Guid userId, string value);
+    Task<CommentDto> CreateCommentAsync(CreateCommentDto createCommentDto);
 
     Task<PagedResult<CommentDto>> GetCommentsByRecipeIdAsync(Guid recipeId, int page = 1, int pageSize = 20,
         DateTime? from = null, DateTime? to = null);
 
-    Task<CommentDto> UpdateCommentAsync(Guid commentId, Guid userId, string value);
+    Task<CommentDto> UpdateCommentAsync(UpdateCommentDto updateCommentDto);
+
     Task DeleteCommentAsync(Guid commentId, Guid userId);
 }
