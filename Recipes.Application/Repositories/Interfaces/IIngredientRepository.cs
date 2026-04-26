d8c6134cd98a8fb2dc6d439cfb80ef30d4596437
antspring <antspring@users.noreply.github.com>
2026-04-26 23:26:31 +0500

    refactor: trim image and ingredient repositories

--- Recipes.Application/Repositories/Interfaces/IIngredientRepository.cs
+++ Recipes.Application/Repositories/Interfaces/IIngredientRepository.cs
@@ -1,13 +1,6 @@
-using Recipes.Domain.Models;
-
 namespace Recipes.Application.Repositories.Interfaces;
 
 public interface IIngredientRepository
 {
-    Task<Ingredient?> GetByIdAsync(Guid id);
-    Task<List<Ingredient>> GetAllAsync();
-    Task AddAsync(Ingredient ingredient);
-    Task UpdateAsync(Ingredient ingredient);
-    Task DeleteAsync(Ingredient ingredient);
     Task<List<Guid>> GetExistingIdsAsync(IEnumerable<Guid> ids);
 }
