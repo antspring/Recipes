1a22b359f2626746d5d73407d4f1f5e8635ad0ba
antspring <antspring@users.noreply.github.com>
2026-04-26 23:30:02 +0500

    refactor: simplify user repository commands

--- Recipes.Application/Repositories/Interfaces/IUserRepository.cs
+++ Recipes.Application/Repositories/Interfaces/IUserRepository.cs
@@ -4,8 +4,8 @@ namespace Recipes.Application.Repositories.Interfaces;
 
 public interface IUserRepository
 {
-    public Task<User> CreateAsync(User user);
+    public Task CreateAsync(User user);
     public Task<User?> GetByUserNameOrEmailAsync(string? userName, string? email);
     public Task<User?> GetByIdAsync(Guid id);
-    public Task<User> UpdateAsync(User user);
+    public Task UpdateAsync(User user);
 }
