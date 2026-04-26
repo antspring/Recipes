2a480c951e81b8b98edf3441fba1a47dc4e12a94
antspring <antspring@users.noreply.github.com>
2026-04-26 20:29:05 +0500

    refactor: narrow unit of work to save changes

--- Recipes.Application/UnitOfWork/Interfaces/IUnitOfWork.cs
+++ Recipes.Application/UnitOfWork/Interfaces/IUnitOfWork.cs
@@ -1,16 +1,6 @@
-using Recipes.Application.Repositories.Interfaces;
 namespace Recipes.Application.UnitOfWork.Interfaces;
 
 public interface IUnitOfWork
 {
-    public IRefreshTokenRepository RefreshTokens { get; }
-    public IUserRepository Users { get; }
-    public IRecipeRepository Recipes { get; }
-    public IImageRepository Images { get; }
-    public IRecipeImageRepository RecipeImages { get; }
-    public IRecipeIngredientRepository RecipeIngredients { get; }
-    public IIngredientRepository Ingredients { get; }
-    public ICommentRepository Comments { get; }
-
     Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
 }
--- Recipes.Infrastructure/UnitOfWork/Implementations/UnitOfWork.cs
+++ Recipes.Infrastructure/UnitOfWork/Implementations/UnitOfWork.cs
@@ -1,28 +1,9 @@
-using Recipes.Application.Repositories.Interfaces;
 using Recipes.Application.UnitOfWork.Interfaces;
 
 namespace Recipes.Infrastructure.UnitOfWork.Implementations;
 
-public class UnitOfWork(
-    BaseDbContext dbContext,
-    IRefreshTokenRepository refreshTokens,
-    IUserRepository users,
-    IRecipeRepository recipes,
-    IImageRepository images,
-    IRecipeImageRepository recipeImages,
-    IRecipeIngredientRepository recipeIngredients,
-    IIngredientRepository ingredients,
-    ICommentRepository comments) : IUnitOfWork
+public class UnitOfWork(BaseDbContext dbContext) : IUnitOfWork
 {
-    public IRefreshTokenRepository RefreshTokens { get; } = refreshTokens;
-    public IUserRepository Users { get; } = users;
-    public IRecipeRepository Recipes { get; } = recipes;
-    public IImageRepository Images { get; } = images;
-    public IRecipeImageRepository RecipeImages { get; } = recipeImages;
-    public IRecipeIngredientRepository RecipeIngredients { get; } = recipeIngredients;
-    public IIngredientRepository Ingredients { get; } = ingredients;
-    public ICommentRepository Comments { get; } = comments;
-
     public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
     {
         return dbContext.SaveChangesAsync(cancellationToken);
